727c079b6f21af7c20085b69452e5660df97779e
KK <lakb248@gmail.com>
2016-02-12 22:17:42 +0800

    update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -72,7 +72,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
     plugins: [
         'karma-jasmine',
@@ -85,6 +85,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   })
 }
